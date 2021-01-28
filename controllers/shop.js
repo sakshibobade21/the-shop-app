c53891f18e7f8f668fe57abf3be3be958d9da37c
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-01-28 15:14:02 +0530

    Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,13 +4,32 @@ const Product = require('../models/product')
 const Order = require('../models/order')
 const PDFDocument = require('pdfkit')
 
+const ITEMS_PER_PAGE = 2
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1
+  console.log('PAGE:', page)
+  let totalItems
+
+  Product.find().countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
+      console.log(page, page + 1, page - 1)
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Shop',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
     })
     .catch(err => {
@@ -38,15 +57,33 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1
+  console.log('PAGE:', page)
+  let totalItems
+
+  Product.find().countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
+      console.log(page, page + 1, page - 1)
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
     })
     .catch(err => {
+      console.log('ERR', err)
       const error = new Error(err)
       error.httpStatusCode = 500
       return next(error)
