61cc67ea967cdab58f9ae5ff5378f2478aae8a32
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-01-30 10:15:40 +0530

    Deleting without reloading page

--- routes/shop.js
+++ routes/shop.js
@@ -23,4 +23,6 @@ router.post('/create-order', isAuth, shopController.postOrder)
 
 router.get('/orders/:orderId', isAuth, shopController.getInvoice)
 
+router.get('/checkout', isAuth, shopController.getCheckout)
+
 module.exports = router
